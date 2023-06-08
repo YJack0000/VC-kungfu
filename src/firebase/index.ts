9148d6256a8ac2268e7148f055ae507609122b10
YJack0000 <jackleagends@gmail.com>
2023-06-08 15:57:48 +0800

    change to .env file

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { firebaseConfig } from "./config";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
